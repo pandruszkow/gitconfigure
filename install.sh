db1fa0451e63aff4553dcf0bc8380a4c3710cc78
Piotr Andruszkow <developer@andruszkow.com>
2021-09-08 17:28:07 +0100

    Improve install script and add a molly-guard for clobbering the system Git config

--- install.sh
+++ install.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
-cp .gitconfig.local.new ~/.gitconfig.local
+
+echo "Warning! The script will overwrite all local .gitconfig. If this is now what you want, press Ctrl+C now to quit."
+echo
+echo "Press Enter to continue"
+read #wait for Enter press
+
+#Force-copy the machine-specific config (overwrites existing)
+cp --force .gitconfig.local.new ~/.gitconfig.local
+
+#Open nano to let user edit machine-specific config
 nano ~/.gitconfig.local
-ln .gitconfig ~
+
+#Force-hardlink the global config. This way, when changes are made to the config on this machine, the hidden repo is reading from the same file and changes can be tracked easily.
+ln --force .gitconfig ~
