eebcf17cea41999d92b4366e4948b4074cac7e6d
Piotr Andruszkow <piotr.andruszkow@fiosgenomics.com>
2021-11-02 15:52:16 +0000

    Install script changes

--- install.sh
+++ install.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
 
-echo "Warning! The script will overwrite all local .gitconfig. If this is now what you want, press Ctrl+C now to quit."
+echo "Warning! The script will overwrite replace all local .gitconfig."
+echo "If this is not what you want, press Ctrl+C now to quit."
 echo
 echo "Press Enter to continue"
-read #wait for Enter press
+read pause #wait for Enter press
+
+#Move aside existing .gitconfig files
+if [ -f ~/.gitconfig ]; then
+	mv ~/.gitconfig ~/.gitconfig.`date --iso-8601`.pre-managed
+fi
+
+#Move aside existing .gitconfig files
+if [ -f "~/.gitconfig.local" ]; then
+	mv "~/.gitconfig.local" "~/.gitconfig.local".`date --iso-8601`.pre-managed
+fi
+
 
 #Force-copy the machine-specific config (overwrites existing)
 cp --force .gitconfig.local.new ~/.gitconfig.local
@@ -11,5 +23,5 @@ cp --force .gitconfig.local.new ~/.gitconfig.local
 #Open nano to let user edit machine-specific config
 nano ~/.gitconfig.local
 
-#Force-hardlink the global config. This way, when changes are made to the config on this machine, the hidden repo is reading from the same file and changes can be tracked easily.
-ln --force .gitconfig ~
+#Hardlink the global config. This way, when changes are made to the config on this machine, the hidden repo is reading from the same file and changes can be tracked easily.
+ln .gitconfig ~/.gitconfig
